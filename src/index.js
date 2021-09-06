65b52ac4764fffac1d1570499d52b6aebe997345
李建国 <lijianguo@qq.com>
2021-09-06 15:47:37 +0800

    delete:删除多余目录

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+/*
+ * @Descripttion: 
+ * @Author: Calvin
+ * @Date: 2021-09-06 15:40:56
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +14,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
